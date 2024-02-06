5675d8f65a67e92331a472d929af2749787ec956
JBibu <javims@jbibu.xyz>
2024-02-06 20:16:02 +0100

    modificado   Principal.java modificado   README.md

--- Principal.java
+++ Principal.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate; // import the LocalDate class
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        fechayhora();
+    }
+
+    public static void fechayhora () {
+        LocalDate myObj = LocalDate.now(); // Create a date object
+        System.out.println(myObj); // Display the current date
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 <ul>Test 1</ul>
 <ul>Test 2</ul>
 <ul>Test 3</ul>
+
+Se ha añadido un método que muestra la fecha y la hora
